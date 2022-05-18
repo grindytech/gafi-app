647683fd6c4e3a85cb12712d7c7084d922eb1216
Hoang Trung <lhtrung307@gmail.com>
2022-05-18 09:46:24 +0700

    Config graphql endpoint base on env

--- src/graphQL/client.ts
+++ src/graphQL/client.ts
@@ -1,7 +1,8 @@
 import { GraphQLClient } from 'graphql-request';
 
-// Temporary using localhost. Should use env var.
-const endpoint = 'http://localhost:3000';
+import config from 'config';
+
+const endpoint = config.GRAPHQL_ENDPOINT || 'http://localhost:3000';
 
 const client = new GraphQLClient(endpoint, { headers: {} });
 export default client;
