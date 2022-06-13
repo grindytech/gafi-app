1b52a5906b6648e9e4ff98735aa193a994b12d4e
Kuro <nhathoang241199@gmail.com>
2022-06-13 16:12:31 +0700

    new sponsored pool

--- src/pages/SponsoredPool/components/SponsoredPoolTable.tsx
+++ src/pages/SponsoredPool/components/SponsoredPoolTable.tsx
@@ -1,10 +1,12 @@
 // Chakra imports
 import {
+  Box,
   Button,
   Icon,
   IconButton,
   Input,
   InputGroup,
+  InputRightAddon,
   InputRightElement,
   Modal,
   ModalBody,
@@ -21,20 +23,24 @@ import {
   Thead,
   Tr,
   useColorModeValue,
+  useToast,
   VStack,
 } from '@chakra-ui/react';
 import { mdiContentCopy } from '@mdi/js';
-import Card from 'components/card/Card';
-import CardBody from 'components/card/CardBody';
-import CardHeader from 'components/card/CardHeader';
-import { shorten } from 'components/utils';
-import { SponsoredPool } from 'graphQL/generates';
 import React, { useState } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import { useTranslation } from 'react-i18next';
-import SkeletonLoadingRow from 'components/SkeletonLoadingRow';
+
 import SponsoredPoolTableRow from './SponsoredPoolTableRow';
+
+import Card from 'components/card/Card';
+import CardBody from 'components/card/CardBody';
+import CardHeader from 'components/card/CardHeader';
 import EmptyRow from 'components/EmptyRow';
+import featureFlag from 'components/FeatureFlags';
+import SkeletonLoadingRow from 'components/SkeletonLoadingRow';
+import { shorten } from 'components/utils';
+import { SponsoredPool } from 'graphQL/generates';
 
 export interface ISponsoredPool {
   id: string | undefined;
@@ -62,87 +68,161 @@ const SponsoredPoolTable = (props: ISponsoredPoolTableProps) => {
   const { t } = useTranslation();
   const { title, captions, sponsoredPools, children, limitRow, isLoading } =
     props;
+  const toast = useToast();
   const textColor = useColorModeValue('gray.700', 'white');
   const [selectedPool, setSelectedPool] = useState<SponsoredPool | undefined>();
   const SkeletonArray = new Array(limitRow).fill(0);
   return (
     <>
-      <Card overflowX={{ sm: 'scroll', xl: 'hidden' }}>
-        <CardHeader p="6px 0px 22px 0px">
-          <Text fontSize="xl" color={textColor} fontWeight="bold">
-            {title}
-          </Text>
-        </CardHeader>
-
-        <CardBody>
-          <Table variant="simple" textAlign="center" color={textColor}>
-            <TableCaption>{children}</TableCaption>
-            <Thead>
-              <Tr my=".8rem" pl="0px" color="gray.400">
-                {React.Children.toArray(
-                  captions.map(caption => (
-                    <Th color="gray.400">{caption.label}</Th>
-                  ))
-                )}
-              </Tr>
-            </Thead>
-            <Tbody justifyContent="flex-start">
-              {React.Children.toArray(
-                !isLoading ? (
-                  !!sponsoredPools.length ? (
-                    sponsoredPools.map(pool => (
-                      <SponsoredPoolTableRow
-                        pool={pool}
-                        onClick={() => setSelectedPool(pool)}
-                      />
+      {featureFlag.isDisplayNewDashboardUI ? (
+        <>
+          <Card p={0} mb={8} mt={4} overflowX={{ sm: 'scroll', xl: 'hidden' }}>
+            <Table variant="simple" textAlign="center" color={textColor}>
+              <Thead>
+                <Tr pl="0px" color="gray.400">
+                  {React.Children.toArray(
+                    captions.map(caption => (
+                      <Th
+                        color="gray.400"
+                        fontSize="md"
+                        lineHeight="10"
+                        fontWeight="normal"
+                        textAlign={
+                          caption.label === 'owner' ? 'left' : 'center'
+                        }
+                        sx={{
+                          textTransform: 'capitalize',
+                        }}
+                      >
+                        {caption.label}
+                      </Th>
                     ))
+                  )}
+                </Tr>
+              </Thead>
+              <Tbody justifyContent="flex-start">
+                {!isLoading ? (
+                  sponsoredPools.length ? (
+                    React.Children.toArray(
+                      sponsoredPools.map(pool => (
+                        <SponsoredPoolTableRow
+                          pool={pool}
+                          onClick={() => setSelectedPool(pool)}
+                        />
+                      ))
+                    )
                   ) : (
                     <EmptyRow columnAmount={6} />
                   )
                 ) : (
-                  SkeletonArray.map(() => (
-                    <SkeletonLoadingRow columnAmount={limitRow} />
-                  ))
-                )
-              )}
-            </Tbody>
-          </Table>
-        </CardBody>
-      </Card>
+                  React.Children.toArray(
+                    SkeletonArray.map(() => (
+                      <SkeletonLoadingRow columnAmount={limitRow} />
+                    ))
+                  )
+                )}
+              </Tbody>
 
+              {/* <TableCaption>{}</TableCaption> */}
+            </Table>
+          </Card>
+          {children}
+        </>
+      ) : (
+        <Card overflowX={{ sm: 'scroll', xl: 'hidden' }}>
+          <CardHeader p="6px 0px 22px 0px">
+            <Text fontSize="xl" color={textColor} fontWeight="bold">
+              {title}
+            </Text>
+          </CardHeader>
+
+          <CardBody>
+            <Table variant="simple" textAlign="center" color={textColor}>
+              <TableCaption>{children}</TableCaption>
+              <Thead>
+                <Tr my=".8rem" pl="0px" color="gray.400">
+                  {React.Children.toArray(
+                    captions.map(caption => (
+                      <Th color="gray.400">{caption.label}</Th>
+                    ))
+                  )}
+                </Tr>
+              </Thead>
+              <Tbody justifyContent="flex-start">
+                {!isLoading ? (
+                  sponsoredPools.length ? (
+                    React.Children.toArray(
+                      sponsoredPools.map(pool => (
+                        <SponsoredPoolTableRow
+                          pool={pool}
+                          onClick={() => setSelectedPool(pool)}
+                        />
+                      ))
+                    )
+                  ) : (
+                    <EmptyRow columnAmount={6} />
+                  )
+                ) : (
+                  React.Children.toArray(
+                    SkeletonArray.map(() => (
+                      <SkeletonLoadingRow columnAmount={limitRow} />
+                    ))
+                  )
+                )}
+              </Tbody>
+            </Table>
+          </CardBody>
+        </Card>
+      )}
       <Modal
         isOpen={!!selectedPool}
         onClose={() => setSelectedPool(undefined)}
         scrollBehavior="inside"
-        size="2xl"
-        closeOnOverlayClick={false}
+        size="xl"
       >
-        <ModalOverlay />
+        <ModalOverlay
+          bg="none"
+          backdropFilter="auto"
+          backdropInvert="80%"
+          backdropBlur="2px"
+        />
         <ModalContent>
-          <ModalHeader>
+          <ModalHeader mb={10} fontWeight="bold" color="primary" fontSize="2xl">
             {selectedPool?.id &&
               t('POOL_POOL_NAME', { poolName: shorten(selectedPool?.id) })}
           </ModalHeader>
-          <ModalCloseButton />
           <ModalBody>
-            <Text mb={4}>{t('SPONSORED_CONTRACTS')}</Text>
+            <Text fontSize="md" fontWeight="normal" mb={4}>
+              {t('SPONSORED_CONTRACTS')}
+            </Text>
             <VStack alignItems="flex-start">
               {React.Children.toArray(
                 selectedPool?.targets.map((target: string) => (
-                  <InputGroup width="300px">
+                  <InputGroup size="lg" w="full">
                     <Input value={shorten(target)} readOnly />
-                    <InputRightElement>
+                    <InputRightAddon
+                      cursor="pointer"
+                      _hover={{
+                        opacity: 0.8,
+                      }}
+                      bg="primary"
+                      color="white"
+                      fontSize="md"
+                      fontWeight="bold"
+                      onClick={() => {
+                        toast({
+                          description: t('COPIED_TO_CLIPBOARD'),
+                          isClosable: true,
+                          status: 'success',
+                        });
+                      }}
+                    >
                       <CopyToClipboard text={target}>
-                        <IconButton
-                          aria-label="copy-button"
-                          icon={
-                            <Icon>
-                              <path d={mdiContentCopy} />
-                            </Icon>
-                          }
-                        />
+                        <Icon aria-label="copy-button" color="white">
+                          <path fill="currentColor" d={mdiContentCopy} />
+                        </Icon>
                       </CopyToClipboard>
-                    </InputRightElement>
+                    </InputRightAddon>
                   </InputGroup>
                 ))
               )}
@@ -150,7 +230,8 @@ const SponsoredPoolTable = (props: ISponsoredPoolTableProps) => {
           </ModalBody>
           <ModalFooter>
             <Button
-              variant="ghost"
+              size="sm"
+              variant="transparent"
               mr={3}
               onClick={() => setSelectedPool(undefined)}
             >
