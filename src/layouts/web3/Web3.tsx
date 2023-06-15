4605647d9fca8ecabe6a9c5e4d14721a251fd161
synasapmob <sonn51280@gmail.com>
2023-06-15 14:34:19 +0700

    (layout) items has been build interface

--- src/layouts/web3/Web3.tsx
+++ src/layouts/web3/Web3.tsx
@@ -1,5 +1,68 @@
+import { Box, Flex, Grid, Heading, IconButton, Text } from '@chakra-ui/react';
 import React from 'react';
 
+import LineAddIcon from 'public/assets/line/add.svg';
+import { Link } from 'react-router-dom';
+import { ListWeb3Project } from 'layouts/default/DefaultWeb3';
+
 export default function Web3() {
-  return <div>Web3</div>;
+  return (
+    <Box padding={48}>
+      <Box textAlign="center" mb={6}>
+        <Heading color="shader.a.900" fontSize="xl" fontWeight="semibold">
+          🛠 Build your first projects!
+        </Heading>
+
+        <Text color="shader.a.500" whiteSpace="pre-line" mt={1}>
+          {`Lorem ipsum dolor sit amet consectetur. Diam bibendum justo
+                sollicitudin rutrum. Neque velit commodo convallis`}
+        </Text>
+      </Box>
+
+      <Grid
+        gridTemplateColumns="repeat(2, 1fr)"
+        color="shader.a.900"
+        border="0.0625rem solid"
+        borderRadius="xl"
+        borderColor="shader.a.400"
+        padding={6}
+        gap={6}
+      >
+        {React.Children.toArray(
+          ListWeb3Project.map(project => (
+            <Link to={project.link}>
+              <Flex
+                justifyContent="space-between"
+                padding={4}
+                alignItems="center"
+                border="0.0625rem solid"
+                borderColor="shader.a.400"
+                borderRadius="xl"
+              >
+                <Box>
+                  <IconButton
+                    variant="unstyled"
+                    minWidth="auto"
+                    height="auto"
+                    aria-label={`icon-${project.title}`}
+                    borderRadius="lg"
+                    padding={1}
+                    color="white"
+                    background={project.background}
+                    icon={project.icon}
+                  />
+
+                  <Text mt={3} fontWeight="medium">
+                    {project.title}
+                  </Text>
+                </Box>
+
+                <LineAddIcon />
+              </Flex>
+            </Link>
+          ))
+        )}
+      </Grid>
+    </Box>
+  );
 }
