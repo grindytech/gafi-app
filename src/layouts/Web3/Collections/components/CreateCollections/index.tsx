4aaca292d23d96e93eac6f1ee3dd68870a2d6f55
Trần Văn Sơn <42969529+synasapmob@users.noreply.github.com>
2023-07-13 16:32:15 +0700

    (Collection) refetch for CreateCollection, Add CollectionToGame

--- src/layouts/Web3/Collections/components/CreateCollections/index.tsx
+++ src/layouts/Web3/Collections/components/CreateCollections/index.tsx
@@ -3,22 +3,25 @@ import { Button, Flex, useDisclosure } from '@chakra-ui/react';
 import CreateCollectionsModal from './CreateCollectionsModal';
 import { UseFormSetValue, useForm } from 'react-hook-form';
 
-import useForceMount from 'hooks/useForceMount';
 import CollectionID from 'components/Collection/CollectionID';
 import SwitchAdmin, {
   TypeSwitchAdmin,
 } from 'components/SwitchAdmin/SwitchAdmin';
+
+import { useOutletContext } from 'react-router-dom';
+import { Web3OutletContextProps } from 'pages/Web3';
 
 export interface CreateCollectionFieldProps extends TypeSwitchAdmin {
   collection_id: string;
 }
 
 export default function CollectionsCreate() {
+  const { collection } = useOutletContext<Web3OutletContextProps>();
+
   const { setValue, getValues, handleSubmit } =
     useForm<CreateCollectionFieldProps>();
 
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { mounting, setMounting } = useForceMount();
 
   return (
     <Flex
@@ -32,7 +35,7 @@ export default function CollectionsCreate() {
         type="Owner"
       />
 
-      <CollectionID setValue={setValue} refetch={mounting} />
+      <CollectionID setValue={setValue} refetch={collection} />
 
       <Button
         isDisabled={isOpen}
@@ -46,7 +49,7 @@ export default function CollectionsCreate() {
 
       {isOpen && (
         <CreateCollectionsModal
-          refetch={setMounting}
+          refetch={collection}
           onClose={onClose}
           getValues={getValues}
         />
