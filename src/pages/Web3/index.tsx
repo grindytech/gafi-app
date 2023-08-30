e36f66231b73f1561b60ff833c5e9a0ccb4198bd
Trần Văn Sơn <42969529+synasapmob@users.noreply.github.com>
2023-08-30 14:15:54 +0700

    (Games) apply feature Step form, and layout

--- src/pages/Web3/index.tsx
+++ src/pages/Web3/index.tsx
@@ -11,7 +11,7 @@ import {
 
 import DefaultWeb3 from 'layouts/DefaultLayout/DefaultWeb3';
 
-import React, { useState } from 'react';
+import React from 'react';
 
 import TabsGame, { TabsGameDataProps } from './Tabs/TabsGame';
 import TabsGamePanel from './Tabs/TabsGame/TabsGamePanel';
@@ -22,6 +22,7 @@ import TabsNFTPanel from './Tabs/TabsNFT/TabsNFTPanel';
 import { isUndefined } from '@polkadot/util';
 import TabsFirstBuild from './Tabs/TabsFirstBuild';
 import ChakraBox from 'components/ChakraBox';
+import { Outlet, useLocation } from 'react-router-dom';
 
 export interface Web3MetaProps {
   game?: TabsGameDataProps[] | undefined;
@@ -30,8 +31,9 @@ export interface Web3MetaProps {
 }
 
 export default function Web3() {
+  const { pathname } = useLocation();
   const [tab, setTab] = React.useState(0);
-  const [meta, setMeta] = useState<Web3MetaProps>({
+  const [meta, setMeta] = React.useState<Web3MetaProps>({
     game: undefined,
     collection: undefined,
     nft: undefined,
@@ -64,108 +66,116 @@ export default function Web3() {
 
   return (
     <>
-      {isLoading && (
-        <Center height="100vh">
-          <CircularProgress isIndeterminate color="primary.a.400" />
-        </Center>
-      )}
+      {pathname === '/web3' ? (
+        <>
+          {isLoading && (
+            <Center height="100vh">
+              <CircularProgress isIndeterminate color="primary.a.400" />
+            </Center>
+          )}
 
-      {isFirstBuild && <TabsFirstBuild />}
+          {isFirstBuild && <TabsFirstBuild />}
 
-      <Box
-        width={isLoading && isEmpty ? 0 : undefined}
-        height={isLoading && isEmpty ? 0 : undefined}
-        hidden={isFirstBuild}
-        overflow="hidden"
-        pb={24}
-      >
-        <DefaultWeb3>
-          <Tabs
-            variant="unstyled"
-            onChange={index => setTab(index)}
-            index={tab}
+          <Box
+            width={isLoading && isEmpty ? 0 : undefined}
+            height={isLoading && isEmpty ? 0 : undefined}
+            hidden={isFirstBuild}
+            overflow="hidden"
+            pb={24}
           >
-            <TabList overflowX="auto" pb={6} gap={4}>
-              {ListTab.map(meta => (
-                <Tab
-                  key={meta.id}
-                  position="relative"
-                  gap={2}
-                  pt={0}
-                  px={8}
-                  pb={4}
-                  color="shader.a.500"
-                  fontWeight="medium"
-                  sx={{
-                    span: {
-                      px: 2,
-                    },
-                  }}
-                  _before={{
-                    content: `''`,
-                    position: 'absolute',
-                    inset: 'auto 0 0 50%',
-                    transform: 'translateX(-50%)',
-                    transitionDuration: 'ultra-slow',
-                    bg: meta.background,
-                    width: 0,
-                    height: '0.125rem',
-                  }}
-                  _selected={{
-                    color: 'white',
-
-                    span: {
-                      borderRadius: 'md',
-                      bg: 'shader.a.800',
-                    },
-
-                    _before: {
-                      width: 'full',
-                    },
-                  }}
-                >
-                  {meta.tab}
-                </Tab>
-              ))}
-            </TabList>
-
-            <TabPanels tabIndex={tab}>
-              {ListTab.map(meta => (
-                <TabPanel key={meta.id} padding={0}>
-                  {meta.id === tab ? (
-                    <ChakraBox
+            <DefaultWeb3>
+              <Tabs
+                variant="unstyled"
+                onChange={index => setTab(index)}
+                index={tab}
+              >
+                <TabList overflowX="auto" pb={6} gap={4}>
+                  {ListTab.map(meta => (
+                    <Tab
                       key={meta.id}
-                      display="grid"
-                      gap={5}
-                      gridTemplateColumns={{
-                        sm: 'repeat(2, 1fr)',
-                        lg: 'repeat(3, 1fr)',
-                        xl: 'repeat(4, 1fr)',
+                      position="relative"
+                      gap={2}
+                      pt={0}
+                      px={8}
+                      pb={4}
+                      color="shader.a.500"
+                      fontWeight="medium"
+                      sx={{
+                        span: {
+                          px: 2,
+                        },
                       }}
-                      initial={{
-                        opacity: 0,
-                        transform: 'translateX(-25%)',
+                      _before={{
+                        content: `''`,
+                        position: 'absolute',
+                        inset: 'auto 0 0 50%',
+                        transform: 'translateX(-50%)',
+                        transitionDuration: 'ultra-slow',
+                        bg: meta.background,
+                        width: 0,
+                        height: '0.125rem',
                       }}
-                      animate={{
-                        opacity: 1,
-                        transform: 'translateX(0%)',
-                      }}
-                      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                      // @ts-ignore
-                      transition={{
-                        type: 'spring',
-                        delay: 0.15,
+                      _selected={{
+                        color: 'white',
+
+                        span: {
+                          borderRadius: 'md',
+                          bg: 'shader.a.800',
+                        },
+
+                        _before: {
+                          width: 'full',
+                        },
                       }}
                     >
-                      {meta.panel}
-                    </ChakraBox>
-                  ) : null}
-                </TabPanel>
-              ))}
-            </TabPanels>
-          </Tabs>
-        </DefaultWeb3>
-      </Box>
+                      {meta.tab}
+                    </Tab>
+                  ))}
+                </TabList>
+
+                <TabPanels tabIndex={tab}>
+                  {ListTab.map(meta => (
+                    <TabPanel key={meta.id} padding={0}>
+                      {meta.id === tab ? (
+                        <ChakraBox
+                          key={meta.id}
+                          display="grid"
+                          gap={5}
+                          gridTemplateColumns={{
+                            sm: 'repeat(2, 1fr)',
+                            lg: 'repeat(3, 1fr)',
+                            xl: 'repeat(4, 1fr)',
+                          }}
+                          initial={{
+                            opacity: 0,
+                            transform: 'translateX(-25%)',
+                          }}
+                          animate={{
+                            opacity: 1,
+                            transform: 'translateX(0%)',
+                          }}
+                          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                          // @ts-ignore
+                          transition={{
+                            type: 'spring',
+                            delay: 0.15,
+                          }}
+                        >
+                          {meta.panel}
+                        </ChakraBox>
+                      ) : null}
+                    </TabPanel>
+                  ))}
+                </TabPanels>
+              </Tabs>
+            </DefaultWeb3>
+          </Box>
+        </>
+      ) : (
+        <Box mt={5} pb={24}>
+          <Outlet />
+        </Box>
+      )}
     </>
   );
 }
